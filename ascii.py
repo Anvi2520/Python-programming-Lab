c3f1154b6b18e584fab8c2008682855a0bacf449
Anvi2520 <69301744+Anvi2520@users.noreply.github.com>
2020-08-06 15:30:03 +0530

    Add files via upload

---
+++ ascii.py
@@ -0,0 +1,3 @@
+#8. Write a program to print the ASCII value of character.
+c = input("Enter a character: ")  
+print("The ASCII value of '" + c + "' is",ord(c))
---
+++ tablef.py
@@ -0,0 +1,7 @@
+#7. Write a program to print the table of a given number using functions.
+num = int(input("Enter the number: "))
+def table(num):
+    print("Multiplication Table of", num)
+    for i in range(1, 11):
+       print(num,"X",i,"=",num * i)
+table(num)
